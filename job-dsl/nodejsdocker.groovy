cc407971a882f984e9ab86ccf18b371e783f7ba9
Jonathan Diaz <54915096+JCDiaz1201@users.noreply.github.com>
2022-03-07 15:32:24 -0600

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -14,9 +14,7 @@ job('NodeJS Docker example') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('jcdiaz1201
-/
-jenkins-nodejs-docker-demo')
+            repositoryName('jcdiaz1201/jenkins-nodejs-docker-demo')
             tag('${GIT_REVISION,length=9}')
             registryCredentials('dockerhub')
             forcePull(false)
